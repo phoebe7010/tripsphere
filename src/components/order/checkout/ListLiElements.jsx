50d7593533b7b513aabcf7b214b0e7f9d7d3dcd9
sejin <neokin7734@naver.com>
2025-03-09 21:50:47 +0900

    250309 박세진 주문결제 리스트요소 컴포넌트 분리

---
+++ src/components/order/checkout/ListLiElements.jsx
@@ -0,0 +1,18 @@
+const LiElement = ({ IconComponent, Title, description }) => {
+  return (
+    <li className="flex items-center justify-start  py-4 pr-5 pl-4 text-sm/6">
+      <div className="ml-4 flex min-w-0 flex-1 gap-2 items-center">
+        <span className="text-gray-400">
+          <IconComponent className="text-2xl" />
+        </span>
+        <span className="font-medium">{Title}</span>
+      </div>
+
+      <div>
+        <span className="font-medium ">{description}</span>
+      </div>
+    </li>
+  );
+};
+
+export default LiElement;
