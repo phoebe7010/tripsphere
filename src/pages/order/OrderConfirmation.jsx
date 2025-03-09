f2e3a76ea725fd9fce934c75919cda34183ff78c
sejin <neokin7734@naver.com>
2025-03-10 02:14:51 +0900

    250309 박세진 주문 결제 페이지 상품목록 추가

--- src/pages/order/OrderConfirmation.jsx
+++ src/pages/order/OrderConfirmation.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { FcApproval, FcCancel, FcClock, FcQuestions } from 'react-icons/fc';
 import { useNavigate } from 'react-router-dom';
+import OrderState from '../../components/order/orderConfirmation/OrderConfirmState';
+import RecommandProduct from '../../components/order/orderConfirmation/RecommandProduct';
 
 const orderInfo = {
   order_id: 1,
@@ -21,50 +22,292 @@ const orderInfo = {
   used_points: 160000,
 };
 
+const products = [
+  {
+    name: '양평 독채 풀빌라 스테이호은',
+    type: 'pension',
+    // location: '양평군, 경기도, 한국',
+    location: {
+      latitude: 123.123,
+      longitude: 123.123,
+      place_name: '양평군, 경기도, 한국',
+    },
+    description: `예약 전 숙소 이용 안내 및 이용 수칙을 반드시 읽어주세요. '호젓한 은신처'를 의미하는 '호은'.`,
+    original_price: 120000,
+
+    discount_rate: 40,
+    final_price: 80000,
+
+    check_in: '0900',
+    check_out: '1200',
+    capacity: {
+      adult: 3,
+      children: 3,
+    },
+
+    services: [
+      { icon: 'tv', text: '최고의 전망' },
+      { icon: 'map', text: '최고의 위치' },
+    ],
+
+    images: [
+      {
+        src: 'https://i.imgur.com/ni513Ct.jpeg',
+        alt: 'Two each of gray, white, and black shirts laying flat.',
+      },
+      {
+        src: 'https://i.imgur.com/q8ifdCe.jpeg',
+        alt: 'Model wearing plain black basic tee.',
+      },
+    ],
+    host: {
+      name: '홍길동',
+      experience: '호텔리어 15년 경력',
+      contact: '010-0000-0000',
+    },
+    rating: 4.8,
+    reviews_count: 15,
+  },
+
+  {
+    name: '양평 독채 풀빌라 스테이호은',
+    type: 'pension',
+    // location: '양평군, 경기도, 한국',
+    location: {
+      latitude: 123.123,
+      longitude: 123.123,
+      place_name: '양평군, 경기도, 한국',
+    },
+    description: `예약 전 숙소 이용 안내 및 이용 수칙을 반드시 읽어주세요. '호젓한 은신처'를 의미하는 '호은'.`,
+    original_price: 120000,
+
+    discount_rate: 40,
+    final_price: 80000,
+
+    check_in: '0900',
+    check_out: '1200',
+    capacity: {
+      adult: 3,
+      children: 3,
+    },
+
+    services: [
+      { icon: 'tv', text: '최고의 전망' },
+      { icon: 'map', text: '최고의 위치' },
+    ],
+
+    images: [
+      {
+        src: 'https://imgur.com/a/PpgWsfW',
+        alt: 'Two each of gray, white, and black shirts laying flat.',
+      },
+      {
+        src: 'https://imgur.com/a/r6B59wX',
+        alt: 'Model wearing plain black basic tee.',
+      },
+    ],
+    host: {
+      name: '홍길동',
+      experience: '호텔리어 15년 경력',
+      contact: '010-0000-0000',
+    },
+    rating: 4.6,
+    reviews_count: 15,
+  },
+
+  {
+    name: '양평 독채 풀빌라 스테이호은',
+    type: 'pension',
+    // location: '양평군, 경기도, 한국',
+    location: {
+      latitude: 123.123,
+      longitude: 123.123,
+      place_name: '양평군, 경기도, 한국',
+    },
+    description: `예약 전 숙소 이용 안내 및 이용 수칙을 반드시 읽어주세요. '호젓한 은신처'를 의미하는 '호은'.`,
+    original_price: 120000,
+
+    discount_rate: 40,
+    final_price: 80000,
+
+    check_in: '0900',
+    check_out: '1200',
+    capacity: {
+      adult: 3,
+      children: 3,
+    },
+
+    services: [
+      { icon: 'tv', text: '최고의 전망' },
+      { icon: 'map', text: '최고의 위치' },
+    ],
+
+    images: [
+      {
+        src: 'https://imgur.com/a/PpgWsfW',
+        alt: 'Two each of gray, white, and black shirts laying flat.',
+      },
+      {
+        src: 'https://imgur.com/a/r6B59wX',
+        alt: 'Model wearing plain black basic tee.',
+      },
+    ],
+    host: {
+      name: '홍길동',
+      experience: '호텔리어 15년 경력',
+      contact: '010-0000-0000',
+    },
+    rating: 4.2,
+    reviews_count: 15,
+  },
+
+  {
+    name: '양평 독채 풀빌라 스테이호은',
+    type: 'pension',
+    // location: '양평군, 경기도, 한국',
+    location: {
+      latitude: 123.123,
+      longitude: 123.123,
+      place_name: '양평군, 경기도, 한국',
+    },
+    description: `예약 전 숙소 이용 안내 및 이용 수칙을 반드시 읽어주세요. '호젓한 은신처'를 의미하는 '호은'.`,
+    original_price: 120000,
+
+    discount_rate: 40,
+    final_price: 80000,
+
+    check_in: '0900',
+    check_out: '1200',
+    capacity: {
+      adult: 3,
+      children: 3,
+    },
+
+    services: [
+      { icon: 'tv', text: '최고의 전망' },
+      { icon: 'map', text: '최고의 위치' },
+    ],
+
+    images: [
+      {
+        src: 'https://imgur.com/a/PpgWsfW',
+        alt: 'Two each of gray, white, and black shirts laying flat.',
+      },
+      {
+        src: 'https://imgur.com/a/r6B59wX',
+        alt: 'Model wearing plain black basic tee.',
+      },
+    ],
+    host: {
+      name: '홍길동',
+      experience: '호텔리어 15년 경력',
+      contact: '010-0000-0000',
+    },
+    rating: 3.8,
+    reviews_count: 15,
+  },
+
+  {
+    name: '양평 독채 풀빌라 스테이호은',
+    type: 'pension',
+    // location: '양평군, 경기도, 한국',
+    location: {
+      latitude: 123.123,
+      longitude: 123.123,
+      place_name: '양평군, 경기도, 한국',
+    },
+    description: `예약 전 숙소 이용 안내 및 이용 수칙을 반드시 읽어주세요. '호젓한 은신처'를 의미하는 '호은'.`,
+    original_price: 120000,
+
+    discount_rate: 40,
+    final_price: 80000,
+
+    check_in: '0900',
+    check_out: '1200',
+    capacity: {
+      adult: 3,
+      children: 3,
+    },
+
+    services: [
+      { icon: 'tv', text: '최고의 전망' },
+      { icon: 'map', text: '최고의 위치' },
+    ],
+
+    images: [
+      {
+        src: 'https://imgur.com/a/PpgWsfW',
+        alt: 'Two each of gray, white, and black shirts laying flat.',
+      },
+      {
+        src: 'https://imgur.com/a/r6B59wX',
+        alt: 'Model wearing plain black basic tee.',
+      },
+    ],
+    host: {
+      name: '홍길동',
+      experience: '호텔리어 15년 경력',
+      contact: '010-0000-0000',
+    },
+    rating: 3.3,
+    reviews_count: 15,
+  },
+
+  {
+    name: '양평 독채 풀빌라 스테이호은',
+    type: 'pension',
+    // location: '양평군, 경기도, 한국',
+    location: {
+      latitude: 123.123,
+      longitude: 123.123,
+      place_name: '양평군, 경기도, 한국',
+    },
+    description: `예약 전 숙소 이용 안내 및 이용 수칙을 반드시 읽어주세요. '호젓한 은신처'를 의미하는 '호은'.`,
+    original_price: 120000,
+
+    discount_rate: 40,
+    final_price: 80000,
+
+    check_in: '0900',
+    check_out: '1200',
+    capacity: {
+      adult: 3,
+      children: 3,
+    },
+
+    services: [
+      { icon: 'tv', text: '최고의 전망' },
+      { icon: 'map', text: '최고의 위치' },
+    ],
+
+    images: [
+      {
+        src: 'https://imgur.com/a/PpgWsfW',
+        alt: 'Two each of gray, white, and black shirts laying flat.',
+      },
+      {
+        src: 'https://imgur.com/a/r6B59wX',
+        alt: 'Model wearing plain black basic tee.',
+      },
+    ],
+    host: {
+      name: '홍길동',
+      experience: '호텔리어 15년 경력',
+      contact: '010-0000-0000',
+    },
+    rating: 1.8,
+    reviews_count: 15,
+  },
+];
+
 const OrderConfirmation = () => {
   const navigate = useNavigate();
 
-  const OrderState = order => {
-    let State;
-    let message;
-
-    switch (order.payment_status) {
-      case 'pending':
-        State = FcClock;
-        message = '결제 대기중입니다.';
-        break;
-      case 'completed':
-        State = FcApproval;
-        message = '결제가 완료되었습니다.';
-        break;
-      case 'canceled':
-        State = FcCancel;
-        message = '결제가 취소 되었습니다.';
-        break;
-      default:
-        State = FcQuestions;
-        message = '정보를 확인할 수 없습니다.';
-    }
-
-    return (
-      <div className="flex flex-col justify-center items-center gap-4 h-96">
-        <State size={50} />
-        <h1 className="text-4xl font-semibold tracking-tight text-gray-900 ">
-          {message}
-        </h1>
-
-        <div className="p-10 text-sm font-semibold">
-          <p>주문 번호 : {orderInfo.order_id}</p>
-        </div>
-      </div>
-    );
-  };
-
   return (
-    <div className="max-w-[1200px] mx-auto py-[40px]">
+    <div className="max-w-[1200px] mx-auto py-[40px] flex flex-col justify-start items-center gap-12">
       <div className="flex flex-col items-center">
         {/* 주문 문구 */}
-        {OrderState(orderInfo)}
+        <OrderState order={orderInfo} />
 
         <div className="flex justify-center gap-x-8">
           {/* 홈으로 이동*/}
@@ -72,7 +315,7 @@ const OrderConfirmation = () => {
             <button
               type="submit"
               onClick={() => navigate('/')}
-              className="rounded-md bg-sky-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-sky-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+              className="rounded-md bg-sky-600 px-3 py-2 text-sm font-semibold text-white hover:shadow-xs hover:bg-sky-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 transition-colors">
               홈으로 이동
             </button>
           </div>
@@ -82,11 +325,17 @@ const OrderConfirmation = () => {
             <button
               type="submit"
               onClick={() => navigate('/mypage')}
-              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white hover:shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 transition-colors">
               마이페이지로 이동
             </button>
           </div>
         </div>
+      </div>
+
+      <div className="w-full overflow-hidden overflow-x-scroll p-3 flex flex-col gap-3">
+        {/* 추천 여행 패키지 목록 */}
+        <h3>추천 여행 패키지</h3>
+        <RecommandProduct products={products} />
       </div>
     </div>
   );
