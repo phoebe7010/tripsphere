21634a4585a019d3d62e6b880d772ecd0cbda188
sejin <neokin7734@naver.com>
2025-03-09 21:53:15 +0900

    250309 박세진 결제페이지  표출데이터 수정

--- src/pages/order/Checkout.jsx
+++ src/pages/order/Checkout.jsx
@@ -1,19 +1,100 @@
-import { BiMap, BiTv } from 'react-icons/bi';
+import { BiTv } from 'react-icons/bi';
 import { CiCircleMinus, CiCirclePlus } from 'react-icons/ci';
 import { FaRegCalendarAlt } from 'react-icons/fa';
 import { FaMapLocationDot } from 'react-icons/fa6';
 import { IoIosMan } from 'react-icons/io';
 import { MdChildCare } from 'react-icons/md';
 import { useNavigate } from 'react-router-dom';
+import LiElement from '../../components/order/checkout/ListLiElements';
+import PrintOrder from '../../components/order/checkout/PrintOrder';
+
+const receiveData = {
+  // 숙박 시설 정보
+  accommodations: {
+    accommodation_id: '숙소 고유번호',
+    name: '숙소 이름',
+    type: '숙소 유형',
+    loaction: {
+      latitude: 123.4512,
+      longitude: 123.123,
+      place_name: '지역 이름',
+    },
+    description: '숙소 설명',
+    original_price: 100000,
+    discount_rate: 5,
+    final_price: 0,
+    check_in: '체크인시간',
+    check_out: '체크아웃시간',
+    capacity: {
+      adults: 2,
+      children: 2,
+    },
+    service: ['제공 서비스1', '제공 서비스2', '제공 서비스3'],
+    images: ['https://imgur.com/a/PpgWsfW', 'https://imgur.com/a/r6B59wX'],
+    host: {
+      name: '숙박 사업주명',
+      experience: '숙박 사업주 경력 기술',
+      contact: '010-0000-0000',
+    },
+    rating: 3,
+    revicews_count: 3,
+  },
+  // 사용자 정보
+  user: {
+    uid: 'user_123',
+    name: '유저명',
+    email: '유저이메일',
+    phone: '유저연락처',
+    points: 1000000,
+  },
+  // 예약정보
+  // 숙박업소는... 흠... 예약한 방의 등급과 갯수로 가격을 책정해야 해야하는데
+  // 그럼 제공해야하는 정보가 달라져야하지 않을까?
+  // 방마다 설치된 침대의 사이즈를 적는다던가
+  // 등급에 따라 묵을 수 있는 인원수를 제공해야할텐데
+  reservation: {
+    check_info: {
+      check_in: '20260101',
+      check_out: '20260103',
+      check_range: 3,
+    },
+    guest_info: {
+      adult: 2,
+      children: 2,
+      guest_total: 4,
+    },
+    order_info: {
+      order_price: 120000,
+      order_fee: 40000,
+      order_total: 160000,
+    },
+  },
+};
+
+const orderData = {
+  uid: 'user_123',
+  accommodation_id: '숙소 고유번호',
+  check_in: '20260101',
+  check_out: '20260103',
+  guest_count: {
+    adults: 2,
+    children: 2,
+  },
+
+  total_price: 100000,
+  payment_state: 'completed',
+  order_data: '20250309',
+  used_points: 0,
+};
 
 const Checkout = () => {
   const navigate = useNavigate();
 
   return (
     <div className="max-w-[1200px] mx-auto px-[20px] py-[40px]">
-      <div className="flex space-y-6 gap-10 py-[30px] max-md:flex-col max-md:items-center">
+      <div className="flex space-y-6 gap-10 py-[30px] max-lg:flex-col max-lg:items-center">
         {/* 주문 결제 정보 */}
-        <div>
+        <div className="flex-10/12 max-lg:w-full">
           <div className="px-4 sm:px-0">
             <h3 className="text-base/7 font-semibold text-gray-900">
               주문 결제
@@ -30,7 +111,7 @@ const Checkout = () => {
                   여행 패키지
                 </dt>
                 <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
-                  가평군/가족/리버뷰/바베큐#29638
+                  {receiveData.accommodations.name}
                 </dd>
               </div>
               <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
@@ -41,47 +122,22 @@ const Checkout = () => {
                   <ul
                     role="list"
                     className="divide-y divide-gray-100 rounded-md border border-gray-200">
-                    <li className="flex items-center justify-start  py-4 pr-5 pl-4 text-sm/6">
-                      <div className="ml-4 flex min-w-0 flex-1 gap-2 items-center">
-                        <span className="text-gray-400">
-                          <FaMapLocationDot className="text-2xl" />
-                        </span>
-                        <span className="font-medium">여행지</span>
-                      </div>
+                    <LiElement
+                      IconComponent={FaMapLocationDot}
+                      Title={'여행지'}
+                      description={'가평군'}
+                    />
 
-                      <div>
-                        <span className="font-medium ">가평군</span>
-                      </div>
-                    </li>
-
-                    <li className="flex items-center justify-between py-4 pr-5 pl-4 text-sm/6">
-                      <div className="flex w-0 flex-1 items-center gap-8">
-                        <div className="ml-4 flex min-w-0 flex-1 gap-2 items-center">
-                          <span className="text-gray-400">
-                            <FaRegCalendarAlt className="text-2xl" />
-                          </span>
-                          <span className="font-medium">시작일자</span>
-                        </div>
-
-                        <div className="flex gap-x-4">
-                          <time className="font-medium">2025-04-01</time>
-                        </div>
-                      </div>
-                    </li>
-
-                    <li className="flex items-center justify-between py-4 pr-5 pl-4 text-sm/6">
-                      <div className="flex w-0 flex-1 items-center">
-                        <div className="ml-4 flex min-w-0 flex-1 gap-2 items-center">
-                          <span className="text-gray-400">
-                            <FaRegCalendarAlt className="text-2xl" />
-                          </span>
-                          <span className="font-medium">종료일자</span>
-                        </div>
-                        <div className="flex gap-x-4">
-                          <time className="font-medium">2025-04-03</time>
-                        </div>
-                      </div>
-                    </li>
+                    <LiElement
+                      IconComponent={FaRegCalendarAlt}
+                      Title={'체크인'}
+                      description={receiveData.reservation.check_info.check_in}
+                    />
+                    <LiElement
+                      IconComponent={FaRegCalendarAlt}
+                      Title={'체크아웃'}
+                      description={receiveData.reservation.check_info.check_out}
+                    />
                   </ul>
                 </dd>
               </div>
@@ -101,10 +157,6 @@ const Checkout = () => {
                           <IoIosMan className="text-2xl" />
                         </span>
                         <span className="font-medium">성인</span>
-                      </div>
-
-                      <div className="flex-1">
-                        <span>3만원</span>
                       </div>
 
                       <div className="flex items-center">
@@ -130,10 +182,6 @@ const Checkout = () => {
                         <span className="font-medium">청소년</span>
                       </div>
 
-                      <div className="flex-1">
-                        <span>3만원</span>
-                      </div>
-
                       <div className="flex items-center">
                         <button type="button">
                           <CiCircleMinus className="text-3xl text-gray-300 hover:text-gray-500" />
@@ -157,7 +205,7 @@ const Checkout = () => {
                   숙박 장소
                 </dt>
                 <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
-                  호텔
+                  {receiveData.accommodations.type}
                 </dd>
               </div>
 
@@ -166,18 +214,14 @@ const Checkout = () => {
                   호스트 연락처
                 </dt>
                 <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
-                  020-0000-0000
+                  {receiveData.accommodations.host.contact}
                 </dd>
               </div>
 
               <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
                 <dt className="text-sm/6 font-medium text-gray-900">소개</dt>
                 <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
-                  Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim
-                  incididunt cillum culpa consequat. Excepteur qui ipsum aliquip
-                  consequat sint. Sit id mollit nulla mollit nostrud in ea
-                  officia proident. Irure nostrud pariatur mollit ad adipisicing
-                  reprehenderit deserunt qui eu.
+                  {receiveData.accommodations.description}
                 </dd>
               </div>
               <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
@@ -186,7 +230,16 @@ const Checkout = () => {
                   <ul
                     role="list"
                     className="divide-y divide-gray-100 rounded-md border border-gray-200">
-                    <li className="flex items-center justify-between py-4 pr-5 pl-4 text-sm/6">
+                    {receiveData.accommodations.service &&
+                      receiveData.accommodations.service.map((ele, index) => (
+                        <LiElement
+                          key={index}
+                          IconComponent={BiTv}
+                          Title={ele}
+                          description={''}
+                        />
+                      ))}
+                    {/* <li className="flex items-center justify-between py-4 pr-5 pl-4 text-sm/6">
                       <div className="flex w-0 flex-1 items-center">
                         <div className="ml-4 flex min-w-0 flex-1 gap-2 items-center">
                           <span className="shrink-0 text-gray-400">
@@ -209,7 +262,7 @@ const Checkout = () => {
                           </span>
                         </div>
                       </div>
-                    </li>
+                    </li> */}
                   </ul>
                 </dd>
               </div>
@@ -218,27 +271,10 @@ const Checkout = () => {
         </div>
 
         {/* 최종 결제 금액 */}
-        <div>
-          <div className="card bg-base-100 w-96 shadow-sm">
-            <div className="card-body">
-              <h2 className="card-title mb-2">최종 결제 금액</h2>
-
-              <div className="flex justify-between py-2">
-                <p>주문 금액</p>
-                <p className="flex justify-end">26200원</p>
-              </div>
-
-              <div className="flex justify-between py-2">
-                <p>포인트 금액</p>
-                <p className="flex justify-end">-26200원</p>
-              </div>
-
-              <div className="border-t border-gray-200">
-                <div className="flex justify-between py-4">
-                  <p>주문 합계 금액</p>
-                  <p className="flex justify-end">26200원</p>
-                </div>
-              </div>
+        <div className="">
+          <div className="sticky card top-15 bg-base-100 w-96 shadow-sm">
+            <form className="card-body">
+              <PrintOrder receiveData={receiveData} />
 
               <div className="card-actions justify-end">
                 <button
@@ -248,7 +284,7 @@ const Checkout = () => {
                   결제하기
                 </button>
               </div>
-            </div>
+            </form>
           </div>
         </div>
       </div>
