bba996c488fbac33bcea372a685144c612a27f7c
hyeran0513 <hr_0513@naver.com>
2025-03-19 11:41:48 +0900

    250319 김혜란 장바구니 문서 ID를 숙소 ID, 유저 ID를 조합한 cartId로 지정

--- src/services/cartService.jsx
+++ src/services/cartService.jsx
@@ -1,50 +1,36 @@
-import {
-  collection,
-  addDoc,
-  getDocs,
-  query,
-  where,
-  updateDoc,
-  doc,
-} from 'firebase/firestore';
+import { getDoc, doc, setDoc, updateDoc } from 'firebase/firestore';
 import { db } from '../firebase/firebaseConfig';
 
 // 장바구니에 이미 있는지 확인
 const checkCartItemExist = async (cartId) => {
-  const cartQuery = query(collection(db, 'carts'), where('id', '==', cartId));
-  const cartSnapshot = await getDocs(cartQuery);
-  return cartSnapshot;
+  const cartDocRef = doc(db, 'carts', cartId);
+  const cartSnapshot = await getDoc(cartDocRef);
+  return cartSnapshot.exists();
 };
 
 // 장바구니 항목 업데이트
 const updateCartItem = async (cartItem, cartId) => {
-  const cartSnapshot = await checkCartItemExist(cartId);
-
-  if (!cartSnapshot.empty) {
-    const existingCartItem = cartSnapshot.docs[0];
-    await updateDoc(doc(db, 'carts', existingCartItem.id), cartItem);
-  }
+  const cartDocRef = doc(db, 'carts', cartId);
+  await updateDoc(cartDocRef, cartItem);
 };
 
 // 장바구니 항목 추가
 const addNewCartItem = async (cartItem, cartId) => {
-  await addDoc(collection(db, 'carts'), {
-    ...cartItem,
-    id: cartId,
-  });
+  const cartDocRef = doc(db, 'carts', cartId);
+  await setDoc(cartDocRef, cartItem);
 };
 
-// 장바구니에 항목 추가
+// 장바구니에 항목 추가 및 업데이트
 export const addCartItem = async (cartItem) => {
   try {
     // 숙소 ID, 유저 ID를 조합한 cartId
     const cartId = `${cartItem.accommodation_id}_${cartItem.user_id}`;
 
-    // 장바구니에 아이템의 포함 여부
-    const cartSnapshot = await checkCartItemExist(cartId);
+    // 장바구니에 아이템이 이미 있는지 확인
+    const cartExists = await checkCartItemExist(cartId);
 
     // 데이터가 있으면 업데이트, 없으면 추가
-    if (!cartSnapshot.empty) {
+    if (cartExists) {
       await updateCartItem(cartItem, cartId);
     } else {
       await addNewCartItem(cartItem, cartId);
